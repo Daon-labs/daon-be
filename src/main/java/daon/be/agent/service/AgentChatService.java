b4c732c2fb2a7792a187fffbc89357270f7e4cee
hanyong <jk07028@naver.com>
2026-05-20 02:05:42 +0900

    [✨Feat/#3] 내부 Agent Chat 테스트 API 및 Spring AI 기본 연결 (#4)
    * :recycle: refactor: 불필요 import 제거 및 application.yaml 환경변수 오타 수정
    
    * :sparkles: feat: OpenAI 기반 채팅 API 구현
    
    - /api/agent/chat 엔드포인트 추가
    - AgentChatService 에서 Spring AI OpenAiChatModel 을 사용하여 응답 생성
    - 테스트 시스템 프롬프트 작성
    - AgentChatRequest 의 사용자 식별 필드를 userId로 변경
    - OpenAI 채팅 모델을 gpt-5-nano 로 설정하고 temperature(1) 옵션 추가
    
    * :sparkles: feat: 외부 모델 응답의 Null/빈 값 처리
    
    * :bug: fix: Agent 채팅 API 요청 메서드를 POST로 수정

--- src/main/java/daon/be/agent/service/AgentChatService.java
+++ src/main/java/daon/be/agent/service/AgentChatService.java
@@ -2,12 +2,53 @@ package daon.be.agent.service;
 
 import daon.be.agent.dto.AgentChatRequest;
 import daon.be.agent.dto.AgentChatResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class AgentChatService {
 
+    private final OpenAiChatModel chatModel;
+
     public AgentChatResponse chat(AgentChatRequest agentChatRequest) {
-        return new AgentChatResponse("Agent 기능 미구현 상태");
+
+        // Message
+        SystemMessage systemMessage = new SystemMessage("""
+                You are a helpful AI assistant.
+                Always answer in Korean.
+                """);
+
+        UserMessage userMessage = new UserMessage(agentChatRequest.message());
+
+        // Prompt
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+
+        // Request/Response
+        ChatResponse chatResponse = chatModel.call(prompt);
+        String chatResponseText = null;
+
+        if (chatResponse != null
+                && chatResponse.getResult() != null
+                && chatResponse.getResult().getOutput() != null) {
+            chatResponseText = chatResponse.getResult().getOutput().getText();
+        }
+        if (chatResponseText == null || chatResponseText.isBlank()) {
+            throw new IllegalStateException("AI 응답 텍스트가 비어 있습니다.");
+        }
+
+
+        log.info("AI 응답: {}", chatResponseText);
+
+        return new AgentChatResponse(chatResponseText);
     }
 }
