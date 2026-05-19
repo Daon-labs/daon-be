9bde4d44e31773c74c157122f2add9abeac2f0b5
yong203 <jk07028@naver.com>
2026-05-20 02:03:24 +0900

    :bug: fix: Agent 채팅 API 요청 메서드를 POST로 수정

--- src/main/java/daon/be/agent/controller/AgentChatController.java
+++ src/main/java/daon/be/agent/controller/AgentChatController.java
@@ -4,7 +4,7 @@ import daon.be.agent.dto.AgentChatRequest;
 import daon.be.agent.dto.AgentChatResponse;
 import daon.be.agent.service.AgentChatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class AgentChatController {
 
     private final AgentChatService agentChatService;
 
-    @GetMapping("/api/agent/chat")
+    @PostMapping("/api/agent/chat")
     public AgentChatResponse chatRequest(@RequestBody String message){
         AgentChatRequest agentChatRequest = new AgentChatRequest(
                 "test",
