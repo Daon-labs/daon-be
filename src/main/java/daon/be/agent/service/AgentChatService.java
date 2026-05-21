03509f523cbfe17ab8bc1c89c17f152e42c5b228
yong203 <jk07028@naver.com>
2026-05-22 03:36:36 +0900

    :recycle: refactor: chatModel -> chatClient 변경 및 planner 연결

--- src/main/java/daon/be/agent/service/AgentChatService.java
+++ src/main/java/daon/be/agent/service/AgentChatService.java
@@ -2,14 +2,17 @@ package daon.be.agent.service;
 
 import daon.be.agent.dto.AgentChatRequest;
 import daon.be.agent.dto.AgentChatResponse;
+import daon.be.agent.planner.AgentPlanner;
+import daon.be.agent.planner.model.AgentPlan;
 import daon.be.agent.planner.model.AgentPlanningRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.messages.SystemMessage;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.ai.chat.messages.SystemMessage;
+//import org.springframework.ai.chat.messages.UserMessage;
+//import org.springframework.ai.chat.model.ChatResponse;
+//import org.springframework.ai.chat.prompt.Prompt;
+//import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,33 +22,45 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AgentChatService {
 
-    private final OpenAiChatModel chatModel;
+//    private final OpenAiChatModel chatModel;
+    private final ChatClient chatClient;
+    private final AgentPlanner agentPlanner;
 
     public AgentChatResponse chatWithoutPlanning(AgentChatRequest agentChatRequest) {
 
-        // Message
-        SystemMessage systemMessage = new SystemMessage("""
-                You are a helpful AI assistant.
-                Always answer in Korean.
-                """);
+//        // Message
+//        SystemMessage systemMessage = new SystemMessage("""
+//                You are a helpful AI assistant.
+//                Always answer in Korean.
+//                """);
+//
+//        UserMessage userMessage = new UserMessage(agentChatRequest.message());
+//
+//        // Prompt
+//        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+//
+//        // Request/Response
+//        ChatResponse chatResponse = chatModel.call(prompt);
 
-        UserMessage userMessage = new UserMessage(agentChatRequest.message());
+        String chatResponseText = chatClient.prompt()
+                .system("You are a helpful AI assistant. Always answer in Korean.")
+                .user(agentChatRequest.message())
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getText();
 
-        // Prompt
-        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
-
-        // Request/Response
-        ChatResponse chatResponse = chatModel.call(prompt);
-        String chatResponseText = null;
-
-        if (chatResponse != null
-                && chatResponse.getResult() != null
-                && chatResponse.getResult().getOutput() != null) {
-            chatResponseText = chatResponse.getResult().getOutput().getText();
-        }
-        if (chatResponseText == null || chatResponseText.isBlank()) {
-            throw new IllegalStateException("AI 응답 텍스트가 비어 있습니다.");
-        }
+//        String chatResponseText = null;
+//
+//        if (chatResponse != null
+//                && chatResponse.getResult() != null
+//                && chatResponse.getResult().getOutput() != null) {
+//            chatResponseText = chatResponse.getResult().getOutput().getText();
+//        }
+//        if (chatResponseText == null || chatResponseText.isBlank()) {
+//            throw new IllegalStateException("AI 응답 텍스트가 비어 있습니다.");
+//        }
 
 
         log.info("AI 응답: {}", chatResponseText);
@@ -59,6 +74,8 @@ public class AgentChatService {
      * <p>최종 LLM 응답을 AgentChatResponse 형식으로 반환
      */
     public AgentChatResponse chat(AgentChatRequest chatRequest){
+
+        // 사용자 정보와 message를 담은 chatRequest를 planner 입력 형식(AgentPlannerRequest)으로 변화
         AgentPlanningRequest planningRequest = AgentPlanningRequest
                 .builder()
                 .message(chatRequest.message())
@@ -66,6 +83,10 @@ public class AgentChatService {
                 .requestedAt(chatRequest.requestedAt())
                 .build();
 
+        // Planner 호출
+        AgentPlan agentPlan = agentPlanner.plan(planningRequest);
+
+        log.info("agentPlan:{}", agentPlan);
         return new AgentChatResponse(null);
     }
 
